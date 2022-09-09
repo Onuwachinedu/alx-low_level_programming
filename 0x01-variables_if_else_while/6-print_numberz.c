392bd436d5ecdbc2bac81f77917c38434654fe4b
Onuwa Chinedu <conuwa6@gmail.com>
2022-09-09 15:56:04 -0700

    Printing numbers 0 - 9 with putchar function

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints all single digits in base 10
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

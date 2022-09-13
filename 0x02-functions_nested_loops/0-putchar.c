1af6caf296fe4735672948b85ca081c5b75551d2
Onuwa Chinedu <conuwa6@gmail.com>
2022-09-13 08:41:58 -0700

    Creating my own header file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "_putchar.h"
+
+/**
+ * print_alphabet - Prints the word _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char b[10] = "_putchar";
+	int a;
+
+	for (a = 0; a < 8; a++)
+	{
+		_putchar(b[a]);
+	}
+	_putchar('\n');
+	return (0);
+}

53acd4c9c101125e575294add13479450e36e6b6
Onuwa Chinedu <conuwa6@gmail.com>
2022-09-19 12:23:22 -0700

    a function that takes a pointer to an int as parameter and updates the value it points to to 98 fixed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - a func that changes the value of a parameter using a pointer
  *
- * @*n: The pointer in focus
+ * @n: The pointer in focus
  *
  * Return: Always 0 (Success)
  *

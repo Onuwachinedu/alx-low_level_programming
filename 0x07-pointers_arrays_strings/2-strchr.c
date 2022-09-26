52c22b8fab08cdca519c9a61d6f48c280e29f58f
Onuwa Chinedu <conuwa6@gmail.com>
2022-09-26 12:24:33 -0700

    Added a function that recreates strchr function in c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * @s: string to be scanned
+ * @c: character to be searched for in s
+ *
+ * Return: returns on first occurrence of cor Null
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (; *(s + i) != '\0'; i++)
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+	}
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
+}

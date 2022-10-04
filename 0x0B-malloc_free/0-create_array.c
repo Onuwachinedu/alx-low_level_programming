1eb285944784f7fd279e6ab5b891b9f6b1d6122c
Onuwa Chinedu <conuwa6@gmail.com>
2022-10-04 08:34:35 -0700

    Added a function that creates an array of chars, and initializes it with a specific char fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /**
- * create_array - a func that creates an array of chars, and init it with a specific char.
+ * create_array - a func that creates an array of chars
  *
  * @size: size of the array
  * @c: storage char

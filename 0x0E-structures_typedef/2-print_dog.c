323b59975fa4084bd31b419d6f1998027a3360ad
Onuwa Chinedu <conuwa6@gmail.com>
2022-10-10 06:05:23 -0700

    I added a function that prints the structure struct dog. fixed

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * print_dog - this function prints a structure "struct dog"
@@ -15,10 +14,12 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
+
 		if (!(d->age))
 			printf("Age: (nil)\n");
 		else
 			printf("Age: %f\n", d->age);
+
 		if (!(d->owner))
 			printf("Owner: (nil)\n");
 		else

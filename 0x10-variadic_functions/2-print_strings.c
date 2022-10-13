1abec94abfe0a5877ba59978b50217c06fe48e7f
Onuwa Chinedu <conuwa6@gmail.com>
2022-10-13 16:39:12 -0700

    I added a function that prints a variable number of strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,32 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - function that prints strings
+ *
+ * @separator: used to separate the strings to be printed
+ * @n: number of strings passed to the function
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+	char *str;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(ap, char *);
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+		if (i < n - 1)
+			if (separator)
+				printf("%s ", separator); 
+	}
+	va_end(ap);
+	printf("\n");
+}

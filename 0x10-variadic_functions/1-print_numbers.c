b02038634b7e174894e6d4068b284bf0083b8559
Onuwa Chinedu <conuwa6@gmail.com>
2022-10-13 07:36:12 -0700

    I added a function that prints numbers, followed by a new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,11 +12,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list ap;
-	
+
 	if (*separator)
 	{
 		va_start(ap, n);
-		
+
 		for (i = 0; i < n; i++)
 		{
 			printf("%d", va_arg(ap, int));
